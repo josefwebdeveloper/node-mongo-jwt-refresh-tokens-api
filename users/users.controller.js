5d52eda9066c9b4dabb1f1a08dfc9a0567168448
JS <josef2011@gmail.com>
2022-10-12 16:25:30 +0300

    u

--- users/users.controller.js
+++ users/users.controller.js
@@ -11,7 +11,7 @@ router.post('/authenticate', authenticateSchema, authenticate);
 router.post('/register', register);
 router.post('/refresh-token', refreshToken);
 router.post('/revoke-token', authorize(), revokeTokenSchema, revokeToken);
-router.get('/', authorize(Role.Admin), getAll);
+router.get('/', getAll);
 router.get('/:id', authorize(), getById);
 router.get('/:id/refresh-tokens', authorize(), getRefreshTokens);
 
